25c843793cd5d3020d15b5f96045fee2b1dbf22c
sshamshiri <sarashamshiri1398@gmail.com>
2023-06-22 19:40:39 +0330

    add widget in form

--- socialnetwork/accounts/forms.py
+++ socialnetwork/accounts/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class UserRegistrationForm(forms.Form):
-    Username = forms.CharField()
-    Email =    forms.EmailField()
-    Password = forms.CharField()
+    Username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    Email =    forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    Password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Your password'}))
 
