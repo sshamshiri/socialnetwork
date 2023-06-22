699e8f55aa476ccb59e92258e60cae451fd8a43f
sshamshiri <sarashamshiri1398@gmail.com>
2023-06-22 18:12:06 +0330

    add url for accounts

--- socialnetwork/socialnetwork/urls.py
+++ socialnetwork/socialnetwork/urls.py
@@ -19,5 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('home.urls'))
+    path('home/', include('home.urls')),
+    path('accounts/', include('accounts.urls')),
+
 ]
