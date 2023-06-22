dee1a9a5e3238935b76a92dc97bb78da4c68f313
sshamshiri <sarashamshiri1398@gmail.com>
2023-06-22 18:28:30 +0330

    add view for app "accounts"

--- socialnetwork/accounts/views.py
+++ socialnetwork/accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
+from .forms import UserRegistrationForm
 
 # Create your views here.
+class RegisterView(View):
+    def get (self,request):
+        form = UserRegistrationForm()
+        return render(request , 'accounts/register.html',{
+            'form':form
+        })
+    def post (self,request):
+        pass
