212c963c73286ce5a4cea3017b3e3df736774e01
sshamshiri <sarashamshiri1398@gmail.com>
2023-06-24 20:38:37 +0330

    add profile url

--- socialnetwork/accounts/urls.py
+++ socialnetwork/accounts/urls.py
@@ -6,7 +6,9 @@ app_name = 'accounts'
 urlpatterns = [
     path('register/',views.UserRegisterView.as_view(), name='user_register'),
     path('login/',views.UserLoginView.as_view(), name='user_login'),
-    path('logout/',views.UserLogoutView.as_view(), name='user_logout')
+    path('logout/',views.UserLogoutView.as_view(), name='user_logout'),
+    path('profile/<int:user_id>',views.UserProfileView.as_view(), name='user_profile'),
+
 
 
 ]
