f2e3efa600ba5dff793ad46fd022873e315fcb07
sshamshiri <sarashamshiri1398@gmail.com>
2023-06-24 12:38:00 +0330

    complate def post

--- socialnetwork/accounts/views.py
+++ socialnetwork/accounts/views.py
@@ -1,13 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.views import View
 from .forms import UserRegistrationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+
 
 # Create your views here.
 class RegisterView(View):
+    form_class = UserRegistrationForm
+
     def get (self,request):
-        form = UserRegistrationForm()
-        return render(request , 'accounts/register.html',{
-            'form':form
-        })
+        form = self.form_class()
+        return render(request , 'accounts/register.html',{'form':form})
+    
     def post (self,request):
-        pass
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd['Username'] , cd['Email'] , cd['Password'])
+            messages.success(request,'You registerd successfully', 'success')
+            return redirect('home:index')
+        return render(request, 'accounts/register.html',{'form':form})
+            
