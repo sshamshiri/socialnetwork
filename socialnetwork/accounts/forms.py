fb10414f933b8d6a41eab1d32496ae52d434a611
sshamshiri <sarashamshiri1398@gmail.com>
2023-06-24 15:02:39 +0330

    Validate for email and username

--- socialnetwork/accounts/forms.py
+++ socialnetwork/accounts/forms.py
@@ -1,8 +1,23 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class UserRegistrationForm(forms.Form):
-    Username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
-    Email =    forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
-    Password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Your password'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    email =    forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Your password'}))
 
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user = User.objects.filter(email = email)
+        if user:
+            raise ValidationError("This email already exists")
+        return email
+    
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        user = User.objects.filter(username = username)
+        if user:
+            raise ValidationError("This username already exists")
+        return username
