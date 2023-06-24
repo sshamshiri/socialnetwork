6fb0fc05b4834606f36771126caad367b18d5b4b
sshamshiri <sarashamshiri1398@gmail.com>
2023-06-24 17:02:46 +0330

    add class UserLoginView in views.py

--- socialnetwork/accounts/views.py
+++ socialnetwork/accounts/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render , redirect
 from django.views import View
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm , UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate , login
 
 
 # Create your views here.
-class RegisterView(View):
+class UserRegisterView(View):
     form_class = UserRegistrationForm
     template_name = 'accounts/register.html'
 
@@ -22,4 +23,25 @@ class RegisterView(View):
             messages.success(request,'You registerd successfully', 'success')
             return redirect('home:index')
         return render(request , self.template_name , {'form':form})
+    
+class UserLoginView(View):
+    form_class = UserLoginForm
+    template_name = 'accounts/login.html'
+
+    def get (self,request):
+        form = self.form_class()
+        return render(request , self.template_name , {'form':form})
+    
+    def post (self,request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                login (request,user)
+                messages.success(request, "you logged in successfully",'success')
+                return redirect('home:index')
+            messages.error(request, "username or password is wrong",'warning')
+        return render(request, self.template_name , {'form':form})
+
             
