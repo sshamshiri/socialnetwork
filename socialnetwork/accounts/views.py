f9f5306e2b77b5ac592cabe27123c26b117a61b0
sshamshiri <sarashamshiri1398@gmail.com>
2023-06-24 18:49:24 +0330

    add def dispatch

--- socialnetwork/accounts/views.py
+++ socialnetwork/accounts/views.py
@@ -1,3 +1,6 @@
+from typing import Any
+from django.http.request import HttpRequest
+from django.http.response import HttpResponseBase
 from django.shortcuts import render , redirect
 from django.views import View
 from .forms import UserRegistrationForm , UserLoginForm
@@ -10,6 +13,11 @@ from django.contrib.auth import authenticate , login , logout
 class UserRegisterView(View):
     form_class = UserRegistrationForm
     template_name = 'accounts/register.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated :
+            return redirect('home:index')
+        return super().dispatch(request, *args, **kwargs)
 
     def get (self,request):
         form = self.form_class()
@@ -27,6 +35,11 @@ class UserRegisterView(View):
 class UserLoginView(View):
     form_class = UserLoginForm
     template_name = 'accounts/login.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated :
+            return redirect('home:index')
+        return super().dispatch(request, *args, **kwargs)
 
     def get (self,request):
         form = self.form_class()
