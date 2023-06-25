ea0c35725e121428c3ac395937426ebf4f1dc9d0
sshamshiri <sarashamshiri1398@gmail.com>
2023-06-25 17:03:02 +0330

    add class PostDeleteView

--- socialnetwork/home/views.py
+++ socialnetwork/home/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.views import View
 from .models import Post
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 
 class HomeView(View):
     def get(self,request):
@@ -13,3 +14,12 @@ class PostDetailView(View):
         post = Post.objects.get(pk=post_id,slug=post_slug)
         return render(request, 'home/detail.html',{'post':post})
     
+class PostDeleteView(LoginRequiredMixin,View):
+    def get(self,request,post_id):
+        post = Post.objects.get(pk=post_id)
+        if post.user.id == request.user.id :
+            post.delete()
+            messages.success(request,'Post deleted successfully','success')
+        else:
+            messages.error(request,"You can't delete this post",'danger')
+        return redirect('home:index')
