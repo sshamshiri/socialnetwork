06b0bf9c5961f45042a7f1441129c5956e5ae157
sshamshiri <sarashamshiri1398@gmail.com>
2023-06-25 13:16:19 +0330

    edit admin.py

--- socialnetwork/home/admin.py
+++ socialnetwork/home/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Post
 
-# Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user','slug','updated')
+    search_fields = ('slug',)
+    list_filter = ('updated',)
+    prepopulated_fields = {'slug':('body',)}
+    raw_id_fields = ('user',)
