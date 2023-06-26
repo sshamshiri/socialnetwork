5e74445543f31fbbba37964964d640350b439303
sshamshiri <sarashamshiri1398@gmail.com>
2023-06-26 17:30:26 +0330

    add reset_password_confirm

--- socialnetwork/accounts/urls.py
+++ socialnetwork/accounts/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('profile/<int:user_id>',views.UserProfileView.as_view(), name='user_profile'),
     path('reset/' , views.UserPasswordResetView.as_view(),name='reset_password'),
     path('reset/done/' , views.UserPasswordResetDoneView.as_view(),name='reset_password_done'),
+    path('confirm/<uidb64>/<token>/' , views.PasswordResetConfirmView.as_view(),name='reset_password_confirm'),
+
 
 ]
