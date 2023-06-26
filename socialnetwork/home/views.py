b5b09e0ad29d29731ed48dcac9d73ee3a726087f
sshamshiri <sarashamshiri1398@gmail.com>
2023-06-26 13:19:36 +0330

    add class PostCreateUpdateView in views.py

--- socialnetwork/home/views.py
+++ socialnetwork/home/views.py
@@ -55,3 +55,20 @@ class PostUpdateView(LoginRequiredMixin,View):
             new_post.save()
             messages.success(request, 'You updated this post','success')
             return redirect("home:post_detail" , post.id , post.slug )
+
+class PostCreateUpdateView(LoginRequiredMixin,View):
+    form_class = PostCreateUpdateForm
+
+    def get(self , request , *args , **kwargs):
+        form = self.form_class
+        return render(request, 'home/create.html' , {'form':form})
+
+    def post(self , request , *args , **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.slug = slugify(form.cleaned_data['body'][:30])
+            new_post.user = request.user
+            new_post.save()
+            messages.success(request,'You created a new post', 'success')
+            return redirect('home:post_detail' , new_post.id , new_post.slug)
