d57daee235ee6cf2ae6d2be520022dd23340260f
sshamshiri <sarashamshiri1398@gmail.com>
2023-06-29 20:00:46 +0330

    add class PostLikeView

--- socialnetwork/home/views.py
+++ socialnetwork/home/views.py
@@ -2,7 +2,7 @@ from typing import Any
 from django.http.request import HttpRequest
 from django.shortcuts import render , redirect , get_object_or_404
 from django.views import View
-from .models import Post , Comment
+from .models import Post , Comment , Vote
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from .forms import PostCreateUpdateForm , CommentCreateForm , CommentReplyForm
@@ -26,11 +26,15 @@ class PostDetailView(View):
     
     def get(self,request,*args,**kwargs):
         comments = self.post_instance.pcomments.filter(is_reply=False)
+        can_like = False
+        if request.user.is_authentication and self.post_instance.user_can_like(request.user):
+            can_like = True
         return render(request, 'home/detail.html',{
             'post':self.post_instance,
             'comments':comments,
             'form':self.form_class,
-            'reply_form':self.form_class_reply
+            'reply_form':self.form_class_reply,
+            'can_like':can_like
             })
     
     @method_decorator(login_required)
@@ -116,3 +120,14 @@ class PostAddReplyView(LoginRequiredMixin,View):
             reply.save()
             messages.success(request,'Your reply submitted successfully','success')
         return redirect('home:post_detail' , post.id , post.slug )
+    
+class PostLikeView(LoginRequiredMixin,View):
+    def get(self, request, post_id):
+        post = get_object_or_404 (Post , id=post_id)
+        like = Vote.objects.filter(post=post , user=request.user)
+        if like.exists():
+            messages.error(request,'You have already liked this post','danger')
+        else:
+            Vote.objects.create(post=post , user=request.user)
+            messages.success(request,'You liked this post','success')
+        return redirect ('home:post_detail' , post.id ,post.slug)
