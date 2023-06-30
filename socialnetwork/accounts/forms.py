56ce3afdc26415914247bb2d1c9c7627c6a6d602
sshamshiri <sarashamshiri1398@gmail.com>
2023-06-30 16:58:37 +0330

    add class EditUserForm

--- socialnetwork/accounts/forms.py
+++ socialnetwork/accounts/forms.py
@@ -2,7 +2,7 @@ from typing import Any
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-
+from .models import Profile
 
 class UserRegistrationForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
@@ -36,3 +36,11 @@ class UserRegistrationForm(forms.Form):
 class UserLoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Your password'}))
+
+
+class EditUserForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = Profile
+        fields = ('age','bio')
